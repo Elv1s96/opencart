939695437c5ed84de9957b66ff673f4d9df7ed6a
Elv1s96 <33173889+Elv1s96@users.noreply.github.com>
2019-05-07 23:15:28 +0300

    final

---
+++ system/storage/cache/5b/5b395352307fe9f66549c496f3f6bc3d16a5932f48732c0c01b4cecc1b0aa350.php
@@ -0,0 +1,64 @@
+<?php
+
+/* default/template/common/column_right.twig */
+class __TwigTemplate_d77f69742229aafe8cef6a29d361686004836a0a25ae290e9e5a1e829ea8db0f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        if ((isset($context["modules"]) ? $context["modules"] : null)) {
+            // line 2
+            echo "<aside id=\"column-right\" class=\"col-sm-3 hidden-xs\">
+  ";
+            // line 3
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable((isset($context["modules"]) ? $context["modules"] : null));
+            foreach ($context['_seq'] as $context["_key"] => $context["module"]) {
+                // line 4
+                echo "  ";
+                echo $context["module"];
+                echo "
+  ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['module'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 6
+            echo "</aside>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/column_right.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 6,  28 => 4,  24 => 3,  21 => 2,  19 => 1,);
+    }
+}
+/* {% if modules %}*/
+/* <aside id="column-right" class="col-sm-3 hidden-xs">*/
+/*   {% for module in modules %}*/
+/*   {{ module }}*/
+/*   {% endfor %}*/
+/* </aside>*/
+/* {% endif %}*/
+/* */
---
+++ system/storage/cache/f6/f6bfc531af808aac4bef77c633d95a3ac3bcc1cafee5e06eab27d79aa5d698d9.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_2e33f5002fb4dce8bd0ae72c69a03e65ad639f1211d4f327d05271dcba75e0fd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
